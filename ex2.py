1186c119fcc5d058e1df40f7c3f320331d31661a
Lucas <lcorte110210@gmail.com>
2026-03-18 10:41:08 -0300

    Programando os exercícios 2 e 3

---
+++ ex2.py
@@ -0,0 +1,6 @@
+a = int(input("Digite o primeiro número: "))
+b = int(input("Digite o segundo número: "))
+print(a, " + ", b, " = ", a + b)
+print(a, " - ", b, " = ", a - b)
+print(a, " * ", b, " = ", a * b)
+print(a, " / ", b, " = ", a / b)
